1bca10c7bf9d3c2b2784b9458d52cd49dfe47b79
r0b <r0b@syn-2603-7080-8f02-2b30-0000-0000-0000-0323.res6.spectrum.com>
2024-10-24 14:35:28 -0400

    feat: work from today

--- exercises/105_threading2.zig
+++ exercises/105_threading2.zig
@@ -80,9 +80,8 @@ pub fn main() !void {
         const handle1 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_plus, 5, count });
         defer handle1.join();
 
-        // Second thread to calculate the minus numbers.
-        ???
-        
+        const handle2 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_minus, 3, count });
+        defer handle2.join();
     }
     // Here we add up the results.
     std.debug.print("PI ≈ {d:.8}\n", .{4 + pi_plus - pi_minus});
